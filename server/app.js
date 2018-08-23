f64ecb7ae927338a50400df42d69e5af214f5250
Josh <joshcho@Joshs-MacBook-Pro.local>
2018-08-23 15:05:22 -0700

    Remove cpu clustering in order to deploy on ec2 micro instance

--- server/app.js
+++ server/app.js
@@ -1,33 +1,23 @@
 require('newrelic');
-const cluster = require('cluster');
 const express = require('express');
 const path = require('path');
 const routes = require('./../routes');
 
 const app = express();
+app.set('port', process.env.PORT || 3001);
 
-if (cluster.isMaster) {
-  const cpuCount = require('os').cpus().length;
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
-  }
-} else {
-  // const app = express();
+app.get('/', (req, res) => {
+  res.redirect('/rooms/1');
+});
 
-  app.set('port', process.env.PORT || 3001);
+app.use(express.static('public/'));
+app.use(express.static('client/dist'));
 
-  app.get('/', (req, res) => {
-    res.redirect('/rooms/1');
-  });
+app.get('/rooms/:id', (req, res) => {
+  const reactPath = path.join(__dirname, '../public/index.html');
+  res.sendFile(reactPath);
+});
 
-  app.use(express.static('public/'));
-  app.use(express.static('client/dist'));
+app.use('/api', routes);
 
-  app.get('/rooms/:id', (req, res) => {
-    const reactPath = path.join(__dirname, '../public/index.html');
-    res.sendFile(reactPath);
-  });
-
-  app.use('/api', routes);
-}
 module.exports = app;
